c5728b8251f3550c2eb6fd7bf57bdfe74c27a80a
centpays-tech <centpaysaws@gmail.com>
2024-06-26 12:44:43 +0530

    callback updated

--- src/controllers/transactionflowcontroller.js
+++ src/controllers/transactionflowcontroller.js
@@ -145,7 +145,7 @@ async function initiateTransaction(req, res) {
 
     // Redirect to token url
     const redirectUrl = `https://centpays.com/v2/ini_payment/${response.token}`;
-    res.redirect(redirectUrl);
+    res.status(200).json( {redirectUrl});
   } catch (error) {
     if (!res.headersSent) {
       res.status(500).json({ error: "Something wrong happened" });
