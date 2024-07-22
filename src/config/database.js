907a587965f3d565d03b01368415ae13257ee320
centpays-tech <centpaysaws@gmail.com>
2024-07-22 13:03:12 +0530

    livetransaction paused

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     autoIndex: false // Disable automatic index creation
   });
