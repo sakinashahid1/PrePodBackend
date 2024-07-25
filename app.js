65bc15aa493df6600604c22d157c23edbd2f9b0e
centpays-tech <centpaysaws@gmail.com>
2024-07-25 18:41:23 +0530

    livetransaction updated

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome! The server is running fine");
+  res.send("Welcome! The server is running");
 });
 
 app.use(bodyParser.json());
